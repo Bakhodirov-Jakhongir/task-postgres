fe831ec182f9ef290d74a359f2ce296d9c765769
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2021-12-24 10:09:48 +0500

    db connection setup

---
+++ db.js
@@ -0,0 +1,29 @@
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+})
+
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+})
+
+client.connect()
+
+module.exports = {
+    Pool , 
+    Client
+}
